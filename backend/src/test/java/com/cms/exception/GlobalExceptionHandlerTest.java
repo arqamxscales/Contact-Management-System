56c4462684a0d61360540331472596c84e953622
arqamxscales <arqamjaved873@gmail.com>
2026-04-28 16:04:28 +0500

    test(backend): expand global exception coverage

--- backend/src/test/java/com/cms/exception/GlobalExceptionHandlerTest.java
+++ backend/src/test/java/com/cms/exception/GlobalExceptionHandlerTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
